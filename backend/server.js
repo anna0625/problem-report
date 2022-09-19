c41ddb003ae8a5ae082acb34ff356adf7a08791a
anna0625 <anna860625@gmail.com>
2022-09-19 13:48:31 +1000

    setup express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  //   res.send("Hello!");
+  //   res.json({ message: "Welcome to support API!" });
+  res.status(200).json({ message: "Welcome to support API!" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
