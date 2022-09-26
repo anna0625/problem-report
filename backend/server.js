972ebef6342695b6ffdade62f7f490ed29605604
anna0625 <anna860625@gmail.com>
2022-09-26 20:39:14 +1000

    added error & exeption handling (middleware)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   //   res.send("Hello!");
@@ -14,5 +18,7 @@ const userRoutes = require("./routes/userRoutes");
 
 // Routes, here we mount /api/users this router
 app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
