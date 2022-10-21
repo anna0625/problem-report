fc2f9c710bddf2a7e363c039d10a10ab6bfa1c82
anna0625 <anna860625@gmail.com>
2022-10-21 11:22:59 +1100

    protect routes and authentication

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const User = require("./userSchema");
 
 // @description : Register a new user
@@ -39,6 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -60,6 +61,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -67,7 +69,26 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @description : Get current user
+// @route : /api/users/me
+// @access : Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
+});
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
--- backend/users/userRoutes.js
+++ backend/users/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("./userController");
+const { registerUser, loginUser, getMe } = require("./userController");
+const { protect } = require("./authMiddleware");
 
 router.post("/", registerUser);
-
 router.post("/login", loginUser);
+router.post("/me", protect, getMe);
 
 module.exports = router;
